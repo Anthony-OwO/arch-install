673b405587287fe52bd048dc4dd2aeee35a4a2db
Ermolaev Petr <like_913@mail.ru>
2020-02-21 17:24:22 +0600

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ function aur {
 	chown -R $username:users /tmp/$1
 	chown -R $username:users /tmp/$1/PKGBUILD
 	cd $1
-	echo $pass | sudo -u $username makepkg -si --noconfirm
+	-u $username makepkg -si --noconfirm
 	cd ..
 	rm -rf $1
 }
@@ -16,29 +16,29 @@ echo '|              Установка драйверов               |'
 echo '--------------------------------------------------'
 
 echo '>> Установка xorg и mesa'
-#echo $pass | sudo pacman -Sy xorg-server xorg-xinit mesa --noconfirm
+#pacman -Sy xorg-server xorg-xinit mesa --noconfirm
 
 mkdir -p /etc/X11/xorg.conf.d
 cd /etc/X11/xorg.conf.d/
 case $ucode in
 	intel)
-		#echo $pass | sudo pacman -S libva libva-intel-driver --noconfirm
+		#pacman -S libva libva-intel-driver --noconfirm
 		;;
 	*)
-		#echo $pass | sudo pacman -S libva libva-mesa-driver --noconfirm
+		#pacman -S libva libva-mesa-driver --noconfirm
 		;;
 esac
 
 #echo 'Тачпад'
-##echo $pass | sudo pacman -Sy xf86-input-synaptics --noconfirm
+##pacman -Sy xf86-input-synaptics --noconfirm
 #wget 'https://raw.githubusercontent.com/like913/arch-install/master/config/X11/10-synaptics.conf'
 
 #echo '>> Установка поддержки файловых систем NTFS exFAT'
-##echo $pass | sudo pacman -Sy ntfs-3g exfat-utils --noconfirm
+##pacman -Sy ntfs-3g exfat-utils --noconfirm
 
 #echo '>> Видеокарта'
 #echo '>intel'
-##echo $pass | sudo pacman -Sy xf86-video-intel --noconfirm
+##pacman -Sy xf86-video-intel --noconfirm
 ## Загрузка готовой конфигурации intel 
 #wget 'https://raw.githubusercontent.com/like913/arch-install/master/config/20-intel.conf'
 #echo ' Настрока DRM'
@@ -46,11 +46,11 @@ esac
 
 #echo '>nvidia'
 ## Установка nvidia-390xx Для видеокарт серии GeForce 400/500 [NVCx и NVDx] примерно из 2010-2011
-##echo $pass | sudo pacman -Sy nvidia-390xx nvidia-390xx-utils lib32-nvidia-390xx-utils opencl-nvidia-390xx nvidia-390xx-settings --noconfirm
+##pacman -Sy nvidia-390xx nvidia-390xx-utils lib32-nvidia-390xx-utils opencl-nvidia-390xx nvidia-390xx-settings --noconfirm
 ## Переключить на дискретную карту nvidia на ноутбуках
 #wget 'https://raw.githubusercontent.com/like913/arch-install/master/config/X11/20-nvidia-prime.conf'
 
-##echo $pass | sudo pacman -Sy nvidia nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia nvidia nvidia-prime nvidia-settings --noconfirm
+##pacman -Sy nvidia nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia nvidia nvidia-prime nvidia-settings --noconfirm
 #echo '>> Настрока DRM nVidia'
 #sed -i 's/MODULES=(/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm /g' /etc/mkinitcpio.conf
 ## Настроить nvidia на ноутбуках с использованием PRIME Render Offload
@@ -71,52 +71,52 @@ echo '--------------------------------------------------'
 #cd ~/
 
 echo '>> Установка sddm'
-echo $pass | sudo pacman -Sy sddm sddm-kcm --noconfirm
-echo $pass | sudo systemctl enable sddm
+pacman -Sy sddm sddm-kcm --noconfirm
+systemctl enable sddm
 
 #echo '>> Настройка SDDM для xrandr'
-##echo $pass | sudo pacman -S xorg-xrandr --noconfirm
+##pacman -S xorg-xrandr --noconfirm
 #echo 'xrandr --auto' >> /usr/share/sddm/scripts/Xsetup
 #echo 'xrandr --dpi 75' >> /usr/share/sddm/scripts/Xsetup
 
 echo '>> Установка настройки сети'
-echo $pass | sudo pacman -Sy networkmanager samba --noconfirm
-echo $pass | sudo systemctl enable NetworkManager
+pacman -Sy networkmanager samba --noconfirm
+systemctl enable NetworkManager
 
 echo '--------------------------------------------------'
 echo '|         Установка Desktop Environment          |'
 echo '--------------------------------------------------'
 #echo '>> Cinnamon'
-##echo $pass | sudo pacman -Sy cinnamon cinnamon-translations --noconfirm
-##echo $pass | sudo pacman -Sy gnomme-terminal
+##pacman -Sy cinnamon cinnamon-translations --noconfirm
+##pacman -Sy gnomme-terminal
 
 #echo '>> Deepin'
-##echo $pass | sudo pacman -Sy deepin --noconfirm
+##pacman -Sy deepin --noconfirm
 
 echo '>> KDE Plasma'
-#echo $pass | sudo pacman -Sy phonon-qt5-vlc --noconfirm
-#echo $pass | sudo pacman -Sy plasma-meta kdebase packagekit-qt5 kde-gtk-config --noconfirm
+#pacman -Sy phonon-qt5-vlc --noconfirm
+#pacman -Sy plasma-meta kdebase packagekit-qt5 kde-gtk-config --noconfirm
 echo '>  Дополнительный софт для KDE'
-#echo $pass | sudo pacman -Sy konsole partitionmanager ark spectacle okular kcalc --noconfirm
-#echo $pass | sudo pacman -Sy aspell aspell-ru --noconfirm
+#pacman -Sy konsole partitionmanager ark spectacle okular kcalc --noconfirm
+#pacman -Sy aspell aspell-ru --noconfirm
 pacman -Rsnc konqueror kwrite --noconfirm
 
 #echo '>> Bluetooth pulseaudio и alsa поддержка звука'
-##echo $pass | sudo pacman -Sy bluez bluez-utils bluedevil pulseaudio-bluetooth alsa-utils pulseaudio-equalizer-ladspa --noconfirm
-#echo $pass | sudo systemctl enable bluetooth.service
+##pacman -Sy bluez bluez-utils bluedevil pulseaudio-bluetooth alsa-utils pulseaudio-equalizer-ladspa --noconfirm
+#systemctl enable bluetooth.service
 
 #echo 'XFCE'
-##echo $pass | sudo pacman -Sy xfce4 xfce4-goodies --noconfirm
+##pacman -Sy xfce4 xfce4-goodies --noconfirm
 
 echo '--------------------------------------------------'
 echo '|        Установка дополнительного софта         |'
 echo '--------------------------------------------------'
 
 echo '>> Настройка папок пользователя'
-#echo $pass | sudo pacman -Sy xdg-user-dirs --noconfirm
+#pacman -Sy xdg-user-dirs --noconfirm
 
-#echo $pass | sudo pacman -Syu
-#echo $pass | sudo pacman -Sy git --noconfirm
+#pacman -Syu
+#pacman -Sy git --noconfirm
 
 echo '>> Установка pacaur'
 aur auracle-git
@@ -133,59 +133,59 @@ mkinitcpio -p linux
 #aur octopi
 
 #echo '>> Установка Double Commander'
-##echo $pass | sudo pacman -S doublecmd-qt5 --noconfirm
+##pacman -S doublecmd-qt5 --noconfirm
 
 #echo '>> Установка Firewall'
-##echo $pass | sudo pacman -Sy ufw gufw --noconfirm
-#echo $pass | sudo systemctl enable ufw.service
+##pacman -Sy ufw gufw --noconfirm
+#systemctl enable ufw.service
 
 #echo '>> Установка LibreOffice'
-##echo $pass | sudo pacman -Sy libreoffice-fresh libreoffice-fresh-ru --noconfirm
+##pacman -Sy libreoffice-fresh libreoffice-fresh-ru --noconfirm
 
 echo '>> Установка firefox'
-echo $pass | sudo pacman -Sy firefox firefox-i18n-ru flashplugin --noconfirm
+pacman -Sy firefox firefox-i18n-ru flashplugin --noconfirm
 
 #echo '>> Установка почтового клиента'
-##echo $pass | sudo pacman -Sy thunderbird thunderbird-i18n-ru --noconfirm
+##pacman -Sy thunderbird thunderbird-i18n-ru --noconfirm
 
 #echo '>> Разработка ПО'
-##echo $pass | sudo pacman -Sy qtcreator cmake kdevelop --noconfirm
+##pacman -Sy qtcreator cmake kdevelop --noconfirm
 
 #echo '>> VirtualBox'
-##echo $pass | sudo pacman -S virtualbox virtualbox-ext-vnc virtualbox-sdk virtualbox-guest-iso --noconfirm
+##pacman -S virtualbox virtualbox-ext-vnc virtualbox-sdk virtualbox-guest-iso --noconfirm
 #modprobe vboxdrv
 
 #echo ">> Установка TeamViewer"
 #aur teamviewer
-#echo $pass | sudo systemctl enable teamviewerd
+#systemctl enable teamviewerd
 
 echo '>> Читалки, просмотр фото, музыки и прочее'
 echo '> Установка программ для обработки с музыки и видео'
-#echo $pass | sudo pacman -Sy vlc cheese --noconfirm
-#echo $pass | sudo pacman -Sy audacious --noconfirm #audacity --noconfirm
-##echo $pass | sudo pacman -Sy kdenlive --noconfirm
+#pacman -Sy vlc cheese --noconfirm
+#pacman -Sy audacious --noconfirm #audacity --noconfirm
+##pacman -Sy kdenlive --noconfirm
 
 echo '> Установка программ для обработки графики'
-##echo $pass | sudo pacman -Sy gimp --noconfirm
+##pacman -Sy gimp --noconfirm
 #pacman  -Sy blender --noconfirm
-#echo $pass | sudo pacman -Sy gwenview --noconfirm
+#pacman -Sy gwenview --noconfirm
 
 #echo '> Установка qbittorrent'
-##echo $pass | sudo pacman -Sy qbittorrent --noconfirm
+##pacman -Sy qbittorrent --noconfirm
 
 #echo '>Прочее'
-##echo $pass | sudo pacman -Sy neofetch baobab okteta --noconfirm
+##pacman -Sy neofetch baobab okteta --noconfirm
 
 #echo '>> Установка wine'
-##echo $pass | sudo pacman -Sy mono --noconfirm
-##echo $pass | sudo pacman -Sy wine wine-mono wine-gecko winetricks --noconfirm
+##pacman -Sy mono --noconfirm
+##pacman -Sy wine wine-mono wine-gecko winetricks --noconfirm
 #aur dxvk-bin
 #winecfg
 #setup_dxvk install
-##echo $pass | sudo pacman -Sy vkd3d lib32-vkd3d --noconfirm
+##pacman -Sy vkd3d lib32-vkd3d --noconfirm
 
 echo '>> Ставим шрифты'
-#echo $pass | sudo pacman -Sy ttf-liberation ttf-dejavu --noconfirm
+#pacman -Sy ttf-liberation ttf-dejavu --noconfirm
 aur ttf-ms-fonts
 aur ttf-tahoma
 
