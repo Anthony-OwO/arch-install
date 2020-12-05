f12fd6780fb22d770019e879a3abadce0b1540a0
Ermolaev Petr <like_913@mail.ru>
2020-12-05 23:16:50 +0600

    move notabug.org

--- install.sh
+++ install.sh
@@ -15,19 +15,11 @@ echo '--------------------------------------------------'
 echo '|              Установка драйверов               |'
 echo '--------------------------------------------------'
 
-echo '>> Установка xorg и mesa'
-#pacman -Sy xorg-server xorg-xinit mesa --noconfirm
+echo '>> Установка xorg, xorg-xinit и mesa'
+pacman -Sy xorg-server xorg-xinit mesa --noconfirm
 
 mkdir -p /etc/X11/xorg.conf.d
 cd /etc/X11/xorg.conf.d/
-case $ucode in
-	intel)
-		#pacman -S libva libva-intel-driver --noconfirm
-		;;
-	*)
-		#pacman -S libva libva-mesa-driver --noconfirm
-		;;
-esac
 
 #echo 'Тачпад'
 ##pacman -Sy xf86-input-synaptics --noconfirm
@@ -50,6 +42,7 @@ esac
 ## Переключить на дискретную карту nvidia на ноутбуках
 #wget 'https://raw.githubusercontent.com/like913/arch-install/master/config/X11/20-nvidia-prime.conf'
 
+## Установка nvidia для новых видеокарт
 ##pacman -Sy nvidia nvidia-utils lib32-nvidia-utils opencl-nvidia lib32-opencl-nvidia nvidia nvidia-prime nvidia-settings --noconfirm
 #echo '>> Настрока DRM nVidia'
 #sed -i 's/MODULES=(/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm /g' /etc/mkinitcpio.conf
@@ -58,10 +51,34 @@ esac
 ## После перезагрузки выполнить sudo nvidia-xconfig для настройки видеокары
 ## sudo nvidia-xconfig --prime необходимо для конфигурирования драйвера в совместимом режиме с картой intel
 
+echo '>> Установка настройки сети'
+pacman -Sy networkmanager samba --noconfirm
+systemctl enable NetworkManager
+
+#echo '>> Bluetooth pulseaudio и alsa поддержка звука'
+##pacman -Sy bluez bluez-utils bluedevil pulseaudio-bluetooth alsa-utils --noconfirm
+#systemctl enable bluetooth.service
+
+#echo '>> VirtualBox'
+##pacman -S virtualbox virtualbox-ext-vnc virtualbox-sdk virtualbox-guest-iso --noconfirm
+#modprobe vboxdrv
+
+echo '>> QEMU'
+echo '> Vertio'
+sed -i 's/MODULES=(/MODULES=(virtio virtio_blk virtio_pci virtio_net /g' /etc/mkinitcpio.conf
+echo '> Установить поддержку комманд QMP'
+pacman -S qemu-guest-agent --noconfirm
+systemctl enable qemu-guest-agent.service
+
 echo '--------------------------------------------------'
 echo '|           Установка Display Manager            |'
 echo '--------------------------------------------------'
 # раскомментировать блок SDDM или LigtDM
+
+#echo '>> Установка gdm' # Рекомендуется только для гном
+#pacman -Sy gdm --noconfirm
+#systemctl enable gdm.service
+
 #echo '>> Установка LightDM'
 #pacman -Sy lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm
 #cd /etc/lightdm/
@@ -71,22 +88,20 @@ echo '--------------------------------------------------'
 #systemctl enable lightdm.service
 #cd ~/
 
+#echo '>> Установка lxdm'
+#pacman -Sy lxdm --noconfirm
+#systemctl enable lxdm.service
+
 echo '>> Установка sddm'
 pacman -Sy sddm sddm-kcm --noconfirm
 systemctl enable sddm
 
-#echo '>> Настройка SDDM для xrandr'
-##pacman -S xorg-xrandr --noconfirm
-#echo 'xrandr --auto' >> /usr/share/sddm/scripts/Xsetup
-#echo 'xrandr --dpi 75' >> /usr/share/sddm/scripts/Xsetup
-
-echo '>> Установка настройки сети'
-pacman -Sy networkmanager samba --noconfirm
-systemctl enable NetworkManager
-
 echo '--------------------------------------------------'
 echo '|         Установка Desktop Environment          |'
 echo '--------------------------------------------------'
+# Прежде чем продолжить желательно установить любой из базовых шрифтов gnu-free-fonts noto-fonts ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid ttf-ibm-plex ttf-liberation
+pacman -Sy gnu-free-fonts --noconfirm
+
 #echo '>> Gnome'
 #pacman -Sy gnome gnome-shell gnome-extra --noconfirm
 
@@ -98,29 +113,33 @@ echo '--------------------------------------------------'
 ##pacman -Sy deepin --noconfirm
 
 echo '>> KDE Plasma'
-#pacman -Sy phonon-qt5-vlc --noconfirm
-#pacman -Sy plasma-meta kdebase packagekit-qt5 kde-gtk-config --noconfirm
+pacman -Sy phonon-qt5-vlc --noconfirm
+#Полная установка kde со всеми приложениями
+#pacman -Sy plasma kde-accessibility kde-applications kde-education kde-games kde-graphics kde-multimedia kde-network kde-system kde-utilities kdepim kdesdk packagekit-qt5 --noconfirm
+
+#Минимальная без приложений
+pacman -Sy plasma-desktop plasma-nm plasma-pa breeze breeze-gtk kde-gtk-config --noconfirm
 echo '>  Дополнительный софт для KDE'
-#pacman -Sy konsole partitionmanager ark spectacle okular kcalc --noconfirm
+pacman -Sy konsole ark spectacle okular kcalc --noconfirm
 #pacman -Sy aspell aspell-ru --noconfirm
-pacman -Rsnc konqueror kwrite --noconfirm
 
-#echo '>> Bluetooth pulseaudio и alsa поддержка звука'
-##pacman -Sy bluez bluez-utils bluedevil pulseaudio-bluetooth alsa-utils pulseaudio-equalizer-ladspa --noconfirm
-#systemctl enable bluetooth.service
-
-#echo 'XFCE'
+#echo '>> XFCE'
 ##pacman -Sy xfce4 xfce4-goodies --noconfirm
 
+#echo '>> LXDE'
+#pacman -Sy lxde --noconfirm
 echo '--------------------------------------------------'
 echo '|        Установка дополнительного софта         |'
 echo '--------------------------------------------------'
 
 echo '>> Настройка папок пользователя'
-#pacman -Sy xdg-user-dirs --noconfirm
+pacman -Sy xdg-user-dirs --noconfirm
 
 #pacman -Syu
-#pacman -Sy git --noconfirm
+pacman -Sy git --noconfirm
+
+echo '>> Установка библиотек для функция pk и ex'
+pacman -Sy p7zip unrar unarchiver lzop lrzip --noconfirm
 
 echo '>> Установка pacaur'
 aur auracle-git
@@ -136,6 +155,9 @@ mkinitcpio -p linux
 #aur alpm_octopi_utils
 #aur octopi
 
+#echo '>> Установка менеджер дисков KDE'
+#pacman -Sy partitionmanager --noconfirm
+
 #echo '>> Установка Double Commander'
 ##pacman -S doublecmd-qt5 --noconfirm
 
@@ -147,32 +169,24 @@ mkinitcpio -p linux
 ##pacman -Sy libreoffice-fresh libreoffice-fresh-ru --noconfirm
 
 echo '>> Установка firefox'
-pacman -Sy firefox firefox-i18n-ru flashplugin --noconfirm
+pacman -Sy firefox firefox-i18n-ru --noconfirm
 
 #echo '>> Установка почтового клиента'
 ##pacman -Sy thunderbird thunderbird-i18n-ru --noconfirm
 
 #echo '>> Разработка ПО'
-##pacman -Sy qtcreator cmake kdevelop --noconfirm
-
-#echo '>> VirtualBox'
-##pacman -S virtualbox virtualbox-ext-vnc virtualbox-sdk virtualbox-guest-iso --noconfirm
-#modprobe vboxdrv
-
-#echo ">> Установка TeamViewer"
-#aur teamviewer
-#systemctl enable teamviewerd
+##pacman -Sy qtcreator cmake --noconfirm
 
 echo '>> Читалки, просмотр фото, музыки и прочее'
 echo '> Установка программ для обработки с музыки и видео'
-#pacman -Sy vlc cheese --noconfirm
-#pacman -Sy audacious --noconfirm #audacity --noconfirm
+pacman -Sy vlc --noconfirm #cheese --noconfirm
+pacman -Sy audacious --noconfirm #audacity --noconfirm
 ##pacman -Sy kdenlive --noconfirm
 
 echo '> Установка программ для обработки графики'
 ##pacman -Sy gimp --noconfirm
 #pacman  -Sy blender --noconfirm
-#pacman -Sy gwenview --noconfirm
+pacman -Sy gwenview --noconfirm
 
 #echo '> Установка qbittorrent'
 ##pacman -Sy qbittorrent --noconfirm
@@ -180,17 +194,11 @@ echo '> Установка программ для обработки графи
 #echo '>Прочее'
 ##pacman -Sy neofetch baobab okteta --noconfirm
 
-#echo '>> Установка wine'
+#echo '>> Установка wine' #требует поддержку x86.
 ##pacman -Sy mono --noconfirm
 ##pacman -Sy wine wine-mono wine-gecko winetricks --noconfirm
 #aur dxvk-bin
-#winecfg
 #setup_dxvk install
 ##pacman -Sy vkd3d lib32-vkd3d --noconfirm
-
-echo '>> Ставим шрифты'
-#pacman -Sy ttf-liberation ttf-dejavu --noconfirm
-aur ttf-ms-fonts
-aur ttf-tahoma
 
 exit
